f89574ec81676919f655091977f45b0cef1843a6
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-12 23:41:20 -0500

    added borrowing and ref example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    let x = 5;
+    let y = &x;
+
+    println!("Value y={}", y);
+    println!("Value y={:?}", y);
+    println!("Address of y={:p}", y);
+    println!("Deref of y={}", *y);
+    // println!("Address of x={:p}", x);
 }
- 
