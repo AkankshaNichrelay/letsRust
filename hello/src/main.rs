f3ee5c750ade44f94c1d94c1c282cb88050a6b2a
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-13 16:06:34 -0500

    borrow vector

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -9,4 +9,16 @@ fn main() {
     println!("Address of y={:p}", y);
     println!("Deref of y={}", *y);
     // println!("Address of x={:p}", x);
+
+    borrow();
 }
+
+fn borrow() {
+    let v = vec![10,20,30];
+    print_vector(&v);
+    println!("{:#?}", v); // can access v here as references can't move the value
+ }
+ 
+ fn print_vector(x: &Vec<i32>) {
+    println!("Inside print_vector function {:?}", x);
+ }
