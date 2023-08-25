dc9d7ba3bb9eddaeb4b30260ffcb26be27f6132d
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-24 22:17:13 -0500

    create struct config

--- a01_minigrep/src/main.rs
+++ a01_minigrep/src/main.rs
@@ -4,19 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("Searching for {}", filename);
+    println!("Searching for {}", config.query);
+    println!("Searching for {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("something went wrong reading the file");
 
     println!("file contents: {}", contents);
 }
 
-fn parse_config(args: &Vec<String>) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone(); // because we dont want ownership of these strings
+    let filename = args[2].clone();
+    // if we wanted to store references to strings, we would need to use lifetimes.
+    Config { query, filename }
 }
