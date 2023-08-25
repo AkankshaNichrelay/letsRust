b284c6125344143297eabe6df5060b866761cbe9
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-24 22:17:45 -0500

    implementation block for config

--- a01_minigrep/src/main.rs
+++ a01_minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("Searching for {}", config.filename);
@@ -20,9 +20,12 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &Vec<String>) -> Config {
-    let query = args[1].clone(); // because we dont want ownership of these strings
-    let filename = args[2].clone();
-    // if we wanted to store references to strings, we would need to use lifetimes.
-    Config { query, filename }
+impl Config {
+    fn new(args: &Vec<String>) -> Config {
+        let query = args[1].clone(); // because we dont want ownership of these strings
+        let filename = args[2].clone();
+        // if we wanted to store references to strings, we would need to use lifetimes.
+        Config { query, filename }
+    }
 }
+
