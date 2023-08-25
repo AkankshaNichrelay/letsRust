eaeb996f29cee359c6dd377caa76b9f118884cd9
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-25 14:37:16 -0500

    search function using TDD

--- a01_minigrep/src/lib.rs
+++ a01_minigrep/src/lib.rs
@@ -7,7 +7,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? operator is used for early exit an function with return type that's compatible
     //  with the value of the ? is used on
     let contents = fs::read_to_string(config.filename)?;
-    println!("file contents: {}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);    
+    }
+
     Ok(())
 }
 
@@ -31,3 +35,31 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;       // importing everything from parent module
+
+    #[test]         // test attribute
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+}
