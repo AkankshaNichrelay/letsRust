35f45f4e340593e6344029c937bff50dc1db8deb
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-24 22:19:09 -0500

    improved error handling

--- a01_minigrep/src/main.rs
+++ a01_minigrep/src/main.rs
@@ -1,10 +1,16 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(
+        |err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        }
+    );
 
     println!("Searching for {}", config.query);
     println!("Searching for {}", config.filename);
@@ -21,15 +27,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            // panic is better for programming error and not user error
+            // panic!("not enough arguments");
+            return Err("not enough arguments")
         }
 
         let query = args[1].clone(); // because we dont want ownership of these strings
         let filename = args[2].clone();
         // if we wanted to store references to strings, we would need to use lifetimes.
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
