b94a5167c345012831cfd717e434da1b101baefb
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-26 12:30:00 -0500

    launching each request in each thread

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener 
@@ -10,8 +12,10 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();   // shadowing
         println!("Connection established!");
-
-        handle_connection(stream);
+        // each requests gets its own thread
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -20,8 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
        ("HTTP/1.1 404 NOT FOUND", "404.html")
