87c4a630782bc6901a31e01c92bc408ed6afb106
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-26 11:44:47 -0500

    path validation

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener 
@@ -15,18 +16,37 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    
+    let mut buffer = [0; 1024]; 
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Request: {}",
-        String::from_utf8_lossy(&buffer[..])
-    );
 
-    // CRLF - carriage return line feed sequence
-    // HTTP-Version Status-Code Reason-Phrase CRLF
-    // headers CRLF
-    // message body
-    //
-    // ex: HTTP/1.1 200 OK\r\n\r\n
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents_index = fs::read_to_string("index.html").unwrap();
+    
+        let response =  format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents_index.len(),
+            contents_index
+        );
+        
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+    } else {
+        let status_line_404 = "HTTP/1.1 404 NOT FOUND";
+        let contents_404 = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line_404,
+            contents_404.len(),
+            contents_404
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+    
 }
